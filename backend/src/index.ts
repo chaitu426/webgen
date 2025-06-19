6cdb0cb2bd100da5efe64f47ea66a63a979c0d84
chaitu426 <interiormern@gmail.com>
2025-06-20 00:06:11 +0530

    solved cors and vercel.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ const app = express();
 // })
 
 app.use(cors({
-	origin: process.env.FRONTEND_URL || "http://localhost:3000", 
+	origin: "https://webgen-three.vercel.app", 
 	credentials: true, 
 }));
 app.use(express.json());
