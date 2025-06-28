b53c939188dc148837b93a279647100f26882549
chaitu426 <interiormern@gmail.com>
2025-06-28 22:11:14 +0530

    add

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import app from "./src/index";
 import connectDB from "./src/config/db";
 const server = async ()=>{
-  const PORT = 3000;
+  const PORT = process.env.PORT||3000;
   // Connect to the database
   await connectDB();
   // Start the server
